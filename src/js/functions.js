433e077472a2183ec60b92400c0c0cc06cdca86f
leo <leodangeli11@github.com>
2019-03-06 23:56:56 -0300

    adicion de slider y correcciones de diseño

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,1 +1,18 @@
-console.log("hello, world");
+// --slider
+
+var slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+      var i;
+      var slides = document.getElementsByClassName("imagenSlider");
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+         slides[slideIndex-1].style.display = "block";
+      }
+      slideIndex++;
+      setTimeout(showSlides,3000);
+}
+
+// Fin Slider
