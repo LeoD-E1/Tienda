2289e3cc886697db94bc0cd27d25090ab5680891
leo <leodangeli11@github.com>
2019-03-11 19:55:59 -0300

    implementacion de firebase, añadiendo login y verificacion de usuario

--- src/js/functions.js
+++ src/js/functions.js
@@ -12,7 +12,119 @@ function showSlides() {
          slides[slideIndex-1].style.display = "block";
       }
       slideIndex++;
+      if(slideIndex > slides.length) {slideIndex = 1}
       setTimeout(showSlides,3000);
 }
 
 // Fin Slider
+
+// ---------------Firebase---------------------->
+
+function registrar(){
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+
+  .then(function(){
+    verificarUser();
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
+}
+
+function Ingreso() {
+
+  let emailIngreso = document.getElementById("emailIngreso").value;
+  let passwordIngreso = document.getElementById("passwordIngreso").value;
+
+  firebase.auth().signInWithEmailAndPassword(emailIngreso, passwordIngreso)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
+
+}
+
+function observador(){
+
+
+  firebase.auth().onAuthStateChanged(function(user) {
+
+    if (user) {
+      // User is signed in.
+      console.log("usuario activo")
+      mostrar(user);
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+
+      console.log(user);
+      console.log("********************");
+      console.log(user.emailVerified);
+      console.log("********************");
+      // ...
+    } else {
+        // User is signed out.
+        console.log("no existe usuario activo")
+        // ...
+      }
+  });
+}
+
+observador();
+
+function mostrar(user) {
+  let user = user;
+  let content = document.getElementById("userRegistrado");
+
+  if (user.user.emailVerified){
+    content.innerHTML = `
+      <h1>Bienvenido!</h1>
+      <button onclick="salir()">Salir</button>
+    `;
+  }
+}
+
+function salir(){
+  firebase.auth().signOut()
+  .then(function(){
+    console.log("saliendo...")
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+}
+
+function verificarUser() {
+
+  var user = firebase.auth().currentUser;
+
+  user.sendEmailVerification()
+  .then(function() {
+    // Email sent.
+    console.log("enviando verificacion...");
+  })
+  .catch(function(error) {
+    // An error happened.
+    console.log(error);
+  });
+}
