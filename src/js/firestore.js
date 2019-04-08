346445c941547ee5c835051c926d85ef811bfd7f
leo <leodangeli11@github.com>
2019-04-08 20:58:22 -0300

    funcion de inicio de sesion completo, solo faltan las paginas

--- src/js/firestore.js
+++ src/js/firestore.js
@@ -12,12 +12,12 @@ var db = firebase.firestore();
 //
 //   var nombre = document.getElementById('nameRegistro').value;
 //   var email = document.getElementById('emailRegistro').value;
-//   var contraseña = document.getElementById('passwordRegistro').value;
+//   var contrasena = document.getElementById('passwordRegistro').value;
 //
 //   db.collection("usuarios").add({
 //       nombre: nombre,
 //       email: email,
-//       contraseña: contraseña
+//       contraseña: contrasena
 //   })
 //   .then(function(docRef) {
 //       console.log("Document written with ID: ", docRef.id);
@@ -30,13 +30,22 @@ var db = firebase.firestore();
 //   });
 // }
 
+function redirect(){
+  location.href="../pages/userPrincipal.html"
+}
+
 function registrar(){
 
   var name = document.getElementById('nameRegistro').value;
   var email = document.getElementById('emailRegistro').value;
   var password = document.getElementById('passwordRegistro').value;
 
+  //Registro en auth
+
     firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(){
+      redirect();
+    })
     .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -46,6 +55,23 @@ function registrar(){
     console.log(errorMessage);
     });
 
+  //Registro en Cloud
+
+  db.collection("usuarios").add({
+        nombre: name,
+        email: email,
+        contraseña: password
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        name = document.getElementById('nameRegistro').value = '';
+        email = document.getElementById('emailRegistro').value = '';
+        password = document.getElementById('passwordRegistro').value = '';
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+
   }
 
 function iniciarSesion(){
@@ -53,16 +79,53 @@ function iniciarSesion(){
   var password = document.getElementById('passwordInicio').value;
 
   firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(
-    function redirect(){
-      location.href="../pages/userPrincipal.html"
-    }
-  )
+  .then(function(){
+    redirect();
+  })
   .catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
+  console.log(errorCode);
+  console.log(errorMessage);
   // ...
   });
+}
 
+
+
+function observador(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('existe usuario activo');
+
+
+      var displayName = user.displayName;
+      var email = user.email;
+
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log(user);
+      // ...
+
+    } else {
+      // User is signed out.
+      console.log("no existe usuario activo");
+    }
+  });
+}
+observador();
+
+function cerrarSesion(){
+  firebase.auth().signOut()
+  .then(function (){
+    console.log("saliendo...");
+    location.href="../pages/iniciar.html"
+  })
+  .catch(function (error){
+    console.log(error);
+  })
 }
