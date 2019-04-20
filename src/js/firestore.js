6ab2fc43d8379a9369d1087cbc586f118718ff5c
leo <leodangeli11@github.com>
2019-04-20 00:04:13 -0300

    intentando agregar google y face como inicio de sesion

--- src/js/firestore.js
+++ src/js/firestore.js
@@ -1,7 +1,23 @@
 
+
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyCLXEGETb6pWNvmukEtYAGv4jTaYyjDpV0",
+//     authDomain: "tiendaonline-5a179.firebaseapp.com",
+//     databaseURL: "https://tiendaonline-5a179.firebaseio.com",
+//     projectId: "tiendaonline-5a179",
+//     storageBucket: "tiendaonline-5a179.appspot.com",
+//     messagingSenderId: "83667291581"
+//   };
+//   firebase.initializeApp(config);
+// </script>
+
+
 firebase.initializeApp({
   apiKey: "AIzaSyCLXEGETb6pWNvmukEtYAGv4jTaYyjDpV0",
   authDomain: "tiendaonline-5a179.firebaseapp.com",
+  databaseURL: "https://tiendaonline-5a179.firebaseio.com",
   projectId: "tiendaonline-5a179"
 });
 
@@ -39,12 +55,12 @@ function registrar(){
   var name = document.getElementById('nameRegistro').value;
   var email = document.getElementById('emailRegistro').value;
   var password = document.getElementById('passwordRegistro').value;
-
+  var user = user;
   //Registro en auth
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(function(){
-      redirect();
+      verificar();
     })
     .catch(function(error) {
     // Handle Errors here.
@@ -72,15 +88,22 @@ function registrar(){
         console.error("Error adding document: ", error);
     });
 
-  }
+}
 
 function iniciarSesion(){
   var email = document.getElementById('emailInicio').value;
   var password = document.getElementById('passwordInicio').value;
 
+
   firebase.auth().signInWithEmailAndPassword(email, password)
   .then(function(){
-    redirect();
+    firebase.auth().onAuthStateChanged(function(user) {
+      if(user.emailVerified){
+        redirect();
+      }else {
+        console.log("Primero verifique su correo");
+      }
+    })
   })
   .catch(function(error) {
   // Handle Errors here.
@@ -92,17 +115,13 @@ function iniciarSesion(){
   });
 }
 
-
-
 function observador(){
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      console.log('existe usuario activo');
-
 
       var displayName = user.displayName;
       var email = user.email;
-
+        console.log(email);
       var emailVerified = user.emailVerified;
       var photoURL = user.photoURL;
       var isAnonymous = user.isAnonymous;
@@ -128,4 +147,41 @@ function cerrarSesion(){
   .catch(function (error){
     console.log(error);
   })
+}
+
+function verificar(){
+
+  var user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(function() {
+      // Email sent.
+      console.log("verificando...");
+    }).catch(function(error) {
+      console.log(error);
+      // An error happened.
+  });
+}
+
+function iniciarGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  provider.addScope('https://www.googleapis.com/auth/admin.directory.customer');
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(error);
+  // ...
+});
 }
